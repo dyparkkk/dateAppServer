1ebf7a89841df435800636d26a9de56d7bebd6ac
dyparkkk <dkdlel1z@naver.com>
2021-05-04 23:19:35 +0900

    bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../schemas/user');
 
 const router = express.Router();
