4138a86b557690cc5ae4beaa6c60d4011e8b0575
dyparkkk <dkdlel1z@naver.com>
2021-05-15 16:38:26 +0900

    msg clone typing v5

--- schemas/chats.js
+++ schemas/chats.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const ChatSchema = new Schema({
     roomID : {type: String, required: true},
     senderID : {type: String, required: true},
-    reciverID : {type: String, required: true},
+    recieverID : {type: String, required: true},
     time: {type: String, default: Date.now},
 });
 
