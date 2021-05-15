7f9c425ca27423089688f6d72d3266d2c8af676b
dyparkkk <dkdlel1z@naver.com>
2021-05-15 17:19:06 +0900

    msg clone typing v8

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next)=>{
     res.status(err.status || 500);
-    res.send(error);
+    res.send(error || 'Error !! ');
 });
 
 server.listen(app.get('port'), () =>{
