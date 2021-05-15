a2d642db891370143ee008c5ba6317720b278300
dyparkkk <dkdlel1z@naver.com>
2021-05-15 16:50:41 +0900

    msg clone typing v7

--- app.js
+++ app.js
@@ -70,10 +70,8 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next)=>{
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'prodoction' ? err : {};
     res.status(err.status || 500);
-    res.send('error');
+    res.send(error);
 });
 
 server.listen(app.get('port'), () =>{
